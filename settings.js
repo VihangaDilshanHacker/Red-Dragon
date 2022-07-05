8cf364159ef71c504c0f904db081d374c8665df8
VihangaDilshanHacker <108748995+VihangaDilshanHacker@users.noreply.github.com>
2022-07-05 21:31:11 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,28 +19,28 @@ global.APIKeys = {
 
 //customize settings\\
 
-global.alivemsg = `My name is RED DRAGON🔥
-My Creator is Kumuthu Prabhasha😎` //Costomize Alive Message (type your message in `` )
+global.alivemsg = `My name is ᴠɪʜᴀɴɢᴀ ᴅɪʟꜱʜᴀɴ✨🥀🔥
+`My Creator is ᴠɪʜᴀɴɢᴀ ᴅɪʟꜱʜᴀɴ✨🥀` //Costomize Alive Message (type your message in `` )
 
-global.owner = ['+380944814219'] //Owner number in aive msg
-global.premium = ['+380944814219'] //Owner Number info
-global.ownernomer = '+380944814219' //Owner Number <<<
+global.owner = ['+94776096667'] //Owner number in aive msg
+global.premium = ['+94776096667'] //Owner Number info
+global.ownernomer = '+94776096667' //Owner Number <<<
 
-global.ownername = '😎𝙆.𝙋𝙧𝙖𝙗𝙝𝙖𝙨𝙝𝙖' //Owner Name
+global.ownername = 'ᴠɪʜᴀɴɢᴀ ᴅɪʟꜱʜᴀɴ✨🥀' //Owner Name
 global.botname = ' ☬RED༒DRAGON࿐' //Bot Name
 
-global.button = '🤭Youtube📍' //Costomize A Button Name In Alive Message
-global.btnurl = 'https://youtu.be/E5C-lrl_g9I' //Change Your Button's URL
+global.button = '🤭Whatsapp📍' //Costomize A Button Name In Alive Message
+global.btnurl = 'https://wa.me/94776096667 //Change Your Button's URL
 
 global.region = 'India, South India, Kerala' //Country Region
 
 global.packname = '☬RED༒DRAGON࿐' //Bot Pacage Name
-global.author = '😎𝙆.𝙋𝙧𝙖𝙗𝙝𝙖𝙨𝙝𝙖❯❯' //Author Name
+global.author = 'ᴠɪʜᴀɴɢᴀ ᴅɪʟꜱʜᴀɴ✨🥀❯❯' //Author Name
 global.prefa = ['','!','.','😂','❤️','💠'] //Command Prefix eg:(.alive ,alive 😂alive)
 global.sessionName = 'session' //Session Name!
 global.sp = '⭔' //A Charactor
 global.dragonemoji = '🧩' // Menu emoji
-global.footer = '©_Kumuthu.' //In list menu
+global.footer = '©_ᴠɪʜᴀɴɢᴀ ᴅɪʟꜱʜᴀɴ✨🥀.' //In list menu
 global.chatbotpre = 'true'  // Chat bot true/false
 
 global.ig = 'https://github.com/prabhasha2006' //Github Account!
